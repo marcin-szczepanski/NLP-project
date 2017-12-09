4e26cf2812c64ba494f6409a0d9b6261f633fe37
marcin-szczepanski <marcin.szczepanski1995@gmail.com>
2017-12-09 23:51:40 +0100

    Add files

---
+++ projekt.py
@@ -0,0 +1,46 @@
+from nltk.tokenize import sent_tokenize
+import os
+
+n = 26
+alignedText = ''
+
+def writingToFile(addr):
+    open(addr, 'w', encoding='utf-8').close()
+    filenames = []
+    for i in range(1, n + 1):
+        filenames.append('aligned' + str(i) + '.txt')
+    with open(addr, 'w', encoding='utf-8') as outfile:
+        for fname in filenames:
+            with open(fname, encoding='utf-8') as infile:
+                for line in infile:
+                    outfile.write(line)
+            os.system('del ' + fname)
+
+
+for i in range(1, n+1):
+    open('textENG.txt', 'w', encoding='utf-8').close()
+    open('textCRT.txt', 'w', encoding='utf-8').close()
+    tmpENG = open(str(i) + '\english.txt', encoding='utf-8').read()
+    tmpCRT = open(str(i) + '\croatian.txt', encoding='utf-8').read()
+    tmpENG = tmpENG.replace('..', '')
+    tmpENG = tmpENG.replace('•', '')
+    tmpCRT = tmpCRT.replace('..', '')
+    tmpCRT = tmpCRT.replace('•', '')
+    sent_tokenize_list_ENG = sent_tokenize(tmpENG)
+    sent_tokenize_list_CRT = sent_tokenize(tmpCRT)
+    textENG = open('textENG.txt', 'w', encoding='utf-8')
+    textCRT = open('textCRT.txt', 'w', encoding='utf-8')
+    for item in sent_tokenize_list_ENG:
+        if not str(item) in ['\n', '\r\n']:
+            textENG.write("%s\n" % item)
+    for item in sent_tokenize_list_CRT:
+        if not str(item) in ['\n', '\r\n']:
+            textCRT.write("%s\n" % item)
+    textCRT.close()
+    textENG.close()
+    os.system('cd > tmp' + str(i) + '.dict')
+    os.system('hunalign\hunalign.exe -text -utf -realign tmp' + str(i) + '.dict textENG.txt textCRT.txt > aligned' + str(i) + '.txt')
+    os.system('del tmp' + str(i) + '.dict')
+
+writingToFile('result.txt')
+
